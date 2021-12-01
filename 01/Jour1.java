ffba28bbf7694ab6025839d1c1f08b449ea0a92d
Adrien <adzerty76@outlook.fr>
2021-12-01 16:15:30 +0100

    Puzzle 2

--- 01/Jour1.java
+++ 01/Jour1.java
@@ -30,5 +30,37 @@ public class Jour1
 			}
 		}catch (Exception e){e.printStackTrace();}
 		System.out.println("Réponse au puzzle 1 : " + nbIncrease);
+
+		lastValue = -1;
+		nbIncrease = 0;
+		int[] tab = new int[2000];
+		int ind = 0;
+
+		try
+		{
+			scFic = new Scanner ( new FileReader ( "./input.txt" ) );
+
+			while ( scFic.hasNextLine() )
+			{
+				tab[ind++] = Integer.parseInt(scFic.nextLine());
+
+			}
+		}catch (Exception e){e.printStackTrace();}
+
+		for(int i = 1; i<tab.length-1; i++){
+			int tmpValue = tab[i-1] + tab[i] + tab[i+1];
+			if(lastValue == -1)
+			{
+				lastValue = tmpValue;
+			}else{
+				if(tmpValue > lastValue)
+				{
+					nbIncrease++;
+				}
+				lastValue = tmpValue;
+			}
+		}
+		
+		System.out.println("Réponse au puzzle 2 : " + nbIncrease);
 	}
 }
