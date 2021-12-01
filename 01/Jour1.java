f593c28de722236f65b694ca4d8fc48d163225d8
Adrien <adzerty76@outlook.fr>
2021-12-01 15:59:15 +0100

    Puzzle 1

--- 01/Jour1.java
+++ 01/Jour1.java
@@ -1,6 +1,34 @@
+import java.io.FileReader;
+import java.io.InputStream;
+import java.util.Scanner;
+
 public class Jour1
 {
 	public static void main(String... args){
-		System.out.println("Hello World!");
+		Scanner scFic;
+		int nbIncrease = 0;
+		int lastValue = -1;
+
+    	try
+		{
+			scFic = new Scanner ( new FileReader ( "./input.txt" ) );
+
+			while ( scFic.hasNextLine() )
+			{
+				int tmpValue = Integer.parseInt(scFic.nextLine());
+				if(lastValue == -1)
+				{
+					lastValue = tmpValue;
+				}else{
+					if(tmpValue > lastValue)
+					{
+						nbIncrease++;
+					}
+					lastValue = tmpValue;
+				}
+
+			}
+		}catch (Exception e){e.printStackTrace();}
+		System.out.println("Réponse au puzzle 1 : " + nbIncrease);
 	}
 }
